449d05eeb7fcc46b33f4270744d9bda77128fb5f
paola <l21590301@sjuanrio.tecnm.mx>
2025-09-25 18:49:09 -0600

    feature: 21590301, Actualizar y eliminar posts

--- app.py
+++ app.py
@@ -69,5 +69,14 @@ def update_post(id):
     categories = Category.query.all()
     return render_template('update_post.html', post=post, categories=categories)
 
+#Eliminar post
+@app.route('/posts/delete/<int:id>')
+def delete_post(id):
+    post = Post.query.get(id)
+    if post:
+        db.session.delete(post)
+        db.session.commit()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
